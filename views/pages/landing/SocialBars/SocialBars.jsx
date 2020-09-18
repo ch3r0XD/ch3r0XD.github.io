a2474010c7acf71d1aa8254333e057f7c03a7412
the cyberbyte <66154594+thecyberbyte@users.noreply.github.com>
2020-09-18 22:47:31 +0530

    Update SocialBars.jsx

--- views/pages/landing/SocialBars/SocialBars.jsx
+++ views/pages/landing/SocialBars/SocialBars.jsx
@@ -7,19 +7,19 @@ export default function SocialBars() {
   return (
     <>
       <div className={cx(classes.bottomSocial, classes.left, classes.icons)}>
-        <a href={'https://github.com/BrychanOdlum'} target={'_blank'}>
+        <a href={'https://github.com/thecyberbyte'} target={'_blank'}>
           <i className={'fab fa-github'} />
         </a>
-        <a href={'https://www.linkedin.com/in/brychan-bennett-odlum-6b820755'} target={'_blank'}>
-          <i className={'fab fa-linkedin'} />
+        <a href={'https://t.me/iamch3r0'} target={'_blank'}>
+          <i className={'fab fa-telegram'} />
         </a>
-        <a href={'mailto:hello@brychan.io'}>
+        <a href={'mailto:sasankreddy58@gmail.com'}>
           <i className={'fas fa-envelope'} />
         </a>
       </div>
       <div className={cx(classes.bottomSocial, classes.right, classes.text)}>
         <p>
-          <a href={'mailto:hello@brychan.io'}>hello@brychan.io</a>
+          <a href={'mailto:sasankreddy58@gmail.com'}>sasank reddy</a>
         </p>
       </div>
     </>
