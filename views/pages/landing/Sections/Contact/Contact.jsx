a06aeb31a09b7d7fcafe83f22e8de34d07de5fe5
the cyberbyte <66154594+thecyberbyte@users.noreply.github.com>
2020-09-18 22:14:20 +0530

    Create Contact.jsx

---
+++ views/pages/landing/Sections/Contact/Contact.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import classes from './Contact.scss';
+import Title from '../../../../components/Title/Title';
+
+export default function Contact() {
+  const p1 = `
+  I am not currently interested in any work opportunities, however, if you'd like to reach out to
+  discuss anything else then please do feel free to drop me a line.
+  `; // TODO: Move this into a config file... Markdown?
+
+  return (
+    <section className={classes.contact} id={'contact'}>
+      <Title title={'📨 Get in touch with me.'} alignRight />
+
+      <div className={classes.contactDetails}>
+        <p>{p1}</p>
+
+        <a href={'mailto:sasankreddy58@gmail.com'}>
+          <button>Say hello!</button>
+        </a>
+      </div>
+    </section>
+  );
+}
